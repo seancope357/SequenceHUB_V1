d0159ed12b73ae8822bb880260f7977537614879
seancope357 <seancope357@gmail.com>
2025-09-19 23:41:20 -0500

    g

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -7,16 +7,16 @@ export const createClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-            const cookieStore = cookies()
+        async get(name: string) {
+            const cookieStore = await cookies()
             return cookieStore.get(name)?.value
           },
-          set(name: string, value: string, options: CookieOptions) {
-            const cookieStore = cookies()
+          async set(name: string, value: string, options: CookieOptions) {
+            const cookieStore = await cookies()
             cookieStore.set(name, value, options)
           },
-          remove(name: string, options: CookieOptions) {
-            const cookieStore = cookies()
+          async remove(name: string, options: CookieOptions) {
+            const cookieStore = await cookies()
             cookieStore.set(name, '', options)
           },
       },
